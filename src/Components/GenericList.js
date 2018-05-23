30abf31283077d2607a6edfb5eac1f74f81d4f68
Chris Fenos <chris.m.fenos@gmail.com>
2018-05-23 13:50:37 -0400

    Merging minkee's branch with previous style branch

---
+++ src/Components/GenericList.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { ListView } from 'react-native';
+import { connect } from 'react-redux';
+import ListItem from './ListItem';
+
+class GenericList extends Component {
+
+    componentWillMount() {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+
+        //this passes in the CoinList.json file via reducer -> state -> connect -> mapstatetoprops
+        this.dataSource = ds.cloneWithRows(this.props.coins);
+
+    }
+
+    renderRow(coin) {
+        //return instance of listitem
+        return <ListItem coin={coin} />
+    }
+
+    render() {
+        return (
+            <ListView dataSource={this.dataSource} renderRow={this.renderRow} />
+        );
+    }
+}
+
+/* Object return will show up to props */
+const mapStateToProps = ({ coins }) => {
+    return { coins }
+}
+
+export default connect(mapStateToProps, null)(GenericList);
+
+/* Going to wrap the library list with the connect function, this will allow us to get state data to put in our props  */
