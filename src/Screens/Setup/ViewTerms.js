96f4f26fa2644af4665a7f2f6e4bba18506f6c1a
Chris Fenos <chris.m.fenos@gmail.com>
2018-05-28 10:50:56 -0400

    Created two wallets in view terms page, one random and one from the auto generarted testrpc list and then transfered eth and printed the changed balance.

--- src/Screens/Setup/ViewTerms.js
+++ src/Screens/Setup/ViewTerms.js
@@ -6,13 +6,77 @@ import { Terms } from './terms';
 import { Button } from 'react-native-elements';
 import { BtnSubmit } from '../../Components/Buttons/BtnSubmit';
 
+import provider from '../../constants/Providers';
+var ethers = require('ethers');
+var utils = ethers.utils;
+var Wallet = ethers.Wallet;
+
 class ViewTerms extends Component {
     static navigationOptions = {
         title: "Terms and Conditions",
         headerStyle: {
-          borderBottomWidth: 0,
+            borderBottomWidth: 0,
         }
     };
+
+    constructor() {
+        super();
+
+        var privateKey = "0x9436a5c70623f162df33b93ca29f9430e55202d157b0355b7cd4761b121c3c4f";
+        var wallet = new Wallet(privateKey);
+        wallet.provider = provider;
+
+        console.log(wallet);
+
+        var walletRandom = Wallet.createRandom();
+        walletRandom.provider = provider;
+        console.log("Address: " + walletRandom.address);
+
+
+        var amount = ethers.utils.parseEther('10.0');
+        var sendPromise = wallet.send(walletRandom.address, amount);
+
+        sendPromise.then(function (transactionHash) {
+            console.log(transactionHash);
+
+            provider.getBalance(wallet.address).then(function (balance) {
+
+                // balance is a BigNumber (in wei); format is as a sting (in ether)
+                var etherString = utils.formatEther(balance);
+
+                console.log("first Balance: " + etherString);
+            });
+
+
+            provider.getBalance(walletRandom.address).then(function (balance) {
+
+                // balance is a BigNumber (in wei); format is as a sting (in ether)
+                var etherString = utils.formatEther(balance);
+
+                console.log("random Balance: " + etherString);
+            });
+
+
+        });
+
+
+        // // These will query the network for appropriate values
+        // var options = {
+        //     //gasLimit: 21000
+        //     //gasPrice: utils.bigNumberify("20000000000")
+        // };
+
+        // var promiseSend = wallet.send(address, amount, options);
+
+        // promiseSend.then(function(transaction) {
+        //     console.log(transaction);
+        // });
+
+
+
+
+
+    }
 
     navigate = () => {
         console.log("hello");
