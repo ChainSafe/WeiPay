32420e259fd00a53382219d43859e0da275c0623
Abdulwasay <abdulwasay_mehar@live.com>
2018-05-29 16:27:18 -0400

    configured to work with android studio

--- android/app/src/main/java/com/reactnavigationdemo/MainApplication.java
+++ android/app/src/main/java/com/reactnavigationdemo/MainApplication.java
@@ -3,7 +3,7 @@ package com.reactnavigationdemo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import org.reactnative.camera.RNCameraPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,9 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNCameraPackage(),
-            new RNCameraPackage(),
-            new VectorIconsPackage()
+            new VectorIconsPackage(),
+            new RNCameraPackage()
       );
     }
 
