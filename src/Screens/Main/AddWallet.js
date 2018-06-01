729511b03dbc0ba22cd00e8b8c958c26c8ec4e65
Abdulwasay <abdulwasay_mehar@live.com>
2018-06-01 16:41:48 -0400

    Working with camera, in add wallet

--- src/Screens/Main/AddWallet.js
+++ src/Screens/Main/AddWallet.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import Camera from 'react-native-camera';
 import { StackNavigator, DrawerNavigator } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 
@@ -17,18 +18,43 @@ class AddWallet extends Component {
     }
   }
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      qrcode: ''
+    }
+  }
+
+  onBarCodeRead = (e) => this.setState({ qrcode: e.data });
+
   render() {
     return (
-      <View>
-        <Text>AddWallet</Text>
-        <Text>AddWallet</Text>
-        <Text>AddWallet</Text>
-        <Text>AddWallet</Text>
-        <Text>AddWallet</Text>
-        <Text>AddWallet</Text>
+      <View style={styles.container}>
+        <Camera
+          style={styles.preview}
+          onBarCodeRead={this.onBarCodeRead}
+          ref={cam => this.camera = cam}
+          aspect={Camera.constants.Aspect.fill}
+        >
+          <Text style={{
+            backgroundColor: 'white'
+          }}>{this.state.qrcode}</Text>
+        </Camera>
       </View>
     )
   }
+
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  }
+});
 export default AddWallet
