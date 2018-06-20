52814ffef461d24585a471df13119dcd80da55dc
Chris Fenos <chris.m.fenos@gmail.com>
2018-06-20 13:50:44 -0400

    Added ios build files, weipay ios build has qr scanner

--- android/app/src/main/java/com/reactnavigationdemo/MainApplication.java
+++ android/app/src/main/java/com/reactnavigationdemo/MainApplication.java
@@ -6,6 +6,8 @@ import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import org.reactnative.camera.RNCameraPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,6 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new RNCameraPackage(),
             new VectorIconsPackage(),
             new RNCameraPackage()
       );
