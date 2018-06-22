3c18a6fa68d59018ced25864ab709a4e2ab54ce8
minkee sung <minkeesung@minkees-Air.fios-router.home>
2018-06-22 13:04:28 -0400

    added redux-persist

--- store.js
+++ store.js
@@ -1,10 +1,22 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import AppReducer from './src/Reducers'
 import thunk from 'redux-thunk'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['NavigationReducer']
+}
+
+const persistedReducer = persistReducer(persistConfig, AppReducer)
 
 const store = createStore(
-    AppReducer,
+    persistedReducer,
     applyMiddleware(thunk)
 );
 
-export default store;
+let persistor = persistStore(store)
+
+export { store, persistor }
