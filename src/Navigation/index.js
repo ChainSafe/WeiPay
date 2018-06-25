cc97d888c57c4e8aa5da3cd48363428bdea0f98a
Abdulwasay <abdulwasay_mehar@live.com>
2018-06-25 08:42:59 -0400

    Navigation Docs Complete

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -3,6 +3,10 @@ import { connect } from "react-redux";
 import { addNavigationHelpers } from "react-navigation";
 import NavigationStack from "./navigationStack";
 
+/**
+ * React-Component
+ * Global Navigation Component used to navigate to different screens within the app
+ */
 class AppNavigation extends Component {
   render() {
     const { navigationState, dispatch } = this.props;
@@ -14,6 +18,10 @@ class AppNavigation extends Component {
   }
 }
 
+/**
+ * Returns the current screen the state is pointing to
+ * @param {Object} state 
+ */
 const mapStateToProps = state => {
   return {
     navigationState: state.NavigationReducer
