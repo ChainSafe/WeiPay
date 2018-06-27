ab10461202f871627d0fc203608815a77f93a4e5
Abdulwasay <abdulwasay_mehar@live.com>
2018-06-27 11:56:10 -0400

    Name Refactored

--- ios/WeiPayTests/WeiPayTests.m
+++ ios/WeiPayTests/WeiPayTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface reactNavigationDemoTests : XCTestCase
+@interface WeiPayTests : XCTestCase
 
 @end
 
-@implementation reactNavigationDemoTests
+@implementation WeiPayTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
