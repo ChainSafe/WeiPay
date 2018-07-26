511f88559b634e2169531c6af5ab9f01e18bc565
Chris Fenos <chris.m.fenos@gmail.com>
2018-07-26 14:12:00 -0400

    unusued code and comments removed

--- src/reducers/wallet/NewWalletSetup.js
+++ src/reducers/wallet/NewWalletSetup.js
@@ -21,14 +21,10 @@ const INITIAL_STATE = {
  */
 
 export default (state = INITIAL_STATE, action) => {
-  // console.log(action);
   switch (action.type) {
     case actions.QRSCANNER_DATA:
-      console.log(action.payload);
       return { ...state, QrData: action.payload };
-    case actions.CREATING_NEW_WALLET:
-      console.log('wallet reducer: ');
-      console.log(action.payload);
+    case actions.CREATING_NEW_WALLET:      
       return { ...state, newWallet: true, wallet: action.payload };
     case actions.NEW_WALLET_NAME:
       return { ...state, walletName: action.payload };
@@ -36,19 +32,11 @@ export default (state = INITIAL_STATE, action) => {
       var current = state.tokens;
       let newTokens = [];
       const index = current.map(token => token.id).indexOf(action.payload.id);
-      if (index === -1) {
-        // add the selected coin to the token list
+      if (index === -1) {      
         newTokens = [...current, action.payload];
-      } else {
-        // Delete the selected coin from the token list
-        newTokens = [...current.slice(0, index), ...current.slice(index + 1)];
-        // current
-        //   .slice(0, index)
-        //   .concat(current.slice(index + 1))
-      }
-      console.log(current);
-
-      // await AsyncStorage.setItem('wallet', { ...state, tokens: current } )
+      } else { 
+        newTokens = [...current.slice(0, index), ...current.slice(index + 1)];     
+      }    
       return { ...state, tokens: newTokens };
     case actions.ADD_TOKEN_INFO:
       return { ...state, current_token: action.payload };
