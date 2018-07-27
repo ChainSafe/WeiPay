ae65ae4961eb317d4348fdec109cc3a60df234d1
Abdulwasay <abdulwasay_mehar@live.com>
2018-07-27 11:51:28 -0400

    Portfolio changed to DIGI WALLET

--- src/screens/main/portfolio/index.js
+++ src/screens/main/portfolio/index.js
@@ -97,7 +97,7 @@ class Portfolio extends Component {
             navigation={this.props.navigation}
           />
         </View>
-        <Text style={styles.textHeader} >DASHBOARD</Text>
+        <Text style={styles.textHeader} >DIGI WALLET []-[]</Text>
 
         <View style={styles.accountValueHeader}>
             <Text style={styles.headerValue}>0$</Text>   
