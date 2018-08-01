0d7b5437af0d8fb4fd0ae21d7e0310965350bd0a
Chris Fenos <chris.m.fenos@gmail.com>
2018-08-01 17:32:39 -0400

    inline styles removed

--- src/components/customPageNavs/CoinSendTabNavigator.js
+++ src/components/customPageNavs/CoinSendTabNavigator.js
@@ -46,22 +46,22 @@ class CoinSendTabNavigator extends Component {
                 <TouchableOpacity 
                     style={styles.headerButton}
                     onPress={this.navigateToSend} >                   
-                    <Text style={[styles.headerSend, { height: '100%'}, this.props.sendActive ? { color: '#12c1a2'} : null ]}>Send</Text>
-                    <View style={this.props.sendActive ? { justifyContent: 'flex-end' , borderBottomColor: '#12c1a2', borderBottomWidth: 1.5, width: '60%'} : null }></View>
+                    <Text style={[styles.headerSend, styles.fullHeight , this.props.sendActive ? styles.greenShade : null ]}>Send</Text>
+                    <View style={this.props.sendActive ? styles.sendLine : null }></View>
                 </TouchableOpacity>
          
                 <TouchableOpacity 
                     style={ styles.headerButton}
                     onPress={this.navigateToHistory}>                   
-                    <Text style={[styles.headerActivity, { height: '100%'}, this.props.activityActive ? { color: '#12c1a2'} : null]}>Activity</Text>
-                    <View style={this.props.activityActive ? { justifyContent: 'flex-end', borderBottomColor: '#12c1a2', borderBottomWidth: 1.5} : null}></View>
+                    <Text style={[styles.headerActivity, styles.fullHeight, this.props.activityActive ? styles.greenShade : null]}>Activity</Text>
+                    <View style={this.props.activityActive ? styles.activityLine : null}></View>
                 </TouchableOpacity>
              
                 <TouchableOpacity 
                     style={styles.headerButton}
                     onPress={this.navigateToReceive}>                                    
-                    <Text style={[styles.headerReceive, { height: '100%'}, this.props.receiveActive ? { color: '#12c1a2' } : null]} >Receive</Text>
-                    <View style={this.props.receiveActive ? { justifyContent: 'flex-end', borderBottomColor: '#12c1a2', borderBottomWidth: 1.5, marginLeft:'20%'} : null}> </View>
+                    <Text style={[styles.headerReceive, styles.fullHeight, this.props.receiveActive ? styles.greenShade : null]} >Receive</Text>
+                    <View style={this.props.receiveActive ? styles.receiveLine : null}> </View>
                 </TouchableOpacity>
             </View>
         );
@@ -100,6 +100,30 @@ const styles = StyleSheet.create({
     headerButton: {
         width: '33%'
     },
+    fullHeight: {
+        height: '100%'
+    },
+    greenShade: {
+        color: '#12c1a2'
+    },
+    sendLine: {
+        justifyContent: 'flex-end' , 
+        borderBottomColor: '#12c1a2', 
+        borderBottomWidth: 1.5, 
+        width: '60%'
+    },
+    activityLine:{
+        justifyContent: 'flex-end', 
+        borderBottomColor: '#12c1a2', 
+        borderBottomWidth: 1.5
+    },
+    receiveLine:{
+        justifyContent: 'flex-end', 
+        borderBottomColor: '#12c1a2', 
+        borderBottomWidth: 1.5, 
+        marginLeft:'20%'
+    },
+
 })
 
 export default CoinSendTabNavigator
