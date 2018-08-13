7198bca00d20c8f3827b9fcf6204ecd65f747560
Chris Fenos <chris.m.fenos@gmail.com>
2018-08-13 16:55:11 -0400

    add token bug fixed

--- src/screens/main/tokens/add/Tokens.js
+++ src/screens/main/tokens/add/Tokens.js
@@ -41,9 +41,9 @@ class Coins extends Component {
           </View>
           <View style={styles.tabNavContainer}>
             <TwoTabNavigator
-              leftTabScreen={'AddCoin'}
+              leftTabScreen={'Coins'}
               leftTabText={'Coins'}
-              rightTabScreen={'AddToken'}
+              rightTabScreen={'Tokens'}
               rightTabText={'Tokens'}
               navigation={this.props.navigation}
             />
@@ -85,12 +85,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fafbfe',
   },
   NavBarButton: {
-    flex: 0.5,
+    flex: 0.65,
     justifyContent: 'center',
     paddingBottom: '2%',
   },
   tabNavContainer: {
-    flex: 0.5,
+    flex: 0.3,
     justifyContent: 'center',
     marginBottom: '2%',
   },
