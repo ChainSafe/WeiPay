a885071ea0e0f71f11256dcc1ba60a30ec568d52
Abdulwasay <abdulwasay_mehar@live.com>
2018-08-31 19:41:17 -0400

    Basic Form Container Layout complete

--- src/screens/main/tokens/add/NewToken.js
+++ src/screens/main/tokens/add/NewToken.js
@@ -23,11 +23,81 @@ const ethers = require('ethers');
 class NewToken extends Component {
     render() {
         return (
-            <Text>Hello there</Text>
+          <SafeAreaView style={styles.safeAreaView}>
+            <View style={styles.mainContainer}>
+              
+    
+              <View style={styles.btnContainer}>
+                <LinearButton
+                  onClickFunction={this.navigate}
+                  buttonText='Add New Token'
+                  customStyles={styles.button}
+                />
+                <View style={styles.footerGrandparentContainer}>
+                    <View style={styles.footerParentContainer} >
+                        <Text style={styles.textFooter} >Powered by ChainSafe </Text>
+                    </View>
+                </View>
+              </View>        
+            </View>
+          </SafeAreaView>
         );
+      }
     }
 
 
-}
-
+/**
+ * Styles
+ */
+const styles = StyleSheet.create({
+    safeAreaView: {
+        flex: 1, 
+        backgroundColor: '#fafbfe'
+    },
+    mainContainer: {
+        flex: 1 ,
+        backgroundColor: '#fafbfe',
+    },
+    NavBarButton: {
+        flex: 0.65, 
+        justifyContent: 'center',
+        paddingBottom: '2%',
+    },
+    tabNavContainer: { 
+        flex: 0.3, 
+        justifyContent: 'center',
+        marginBottom: '2%',
+    },
+    coinListContainer: {
+        alignItems: 'stretch',
+        marginLeft: '9%',
+        marginRight: '9%',
+        flex:5,
+        paddingBottom: "2.5%",
+        paddingTop: "2.5%",
+    },
+    btnContainer: {
+        flex: 1,
+        alignItems: 'stretch',
+        justifyContent: 'flex-end',
+    },
+    button: {
+        width: '82%',
+        height: Dimensions.get('window').height * 0.082,  
+    },
+    footerGrandparentContainer: {
+        alignItems: 'center',
+        marginBottom: '3%',
+        marginTop: '3%',
+    },
+    footerParentContainer: {
+        alignItems: 'center',
+    },
+    textFooter: {
+        fontFamily: 'WorkSans-Regular',
+        fontSize: RF(1.7),
+        color: '#c0c0c0',
+        letterSpacing: 0.5
+    },
+    });
 export default NewToken;
