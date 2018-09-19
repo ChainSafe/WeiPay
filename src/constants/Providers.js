b94bcd85728de94831dcbd8a02f7fe11434e4a71
Chris Fenos <chris.m.fenos@gmail.com>
2018-09-19 16:11:05 -0400

    ios crashing after re opening

--- src/constants/Providers.js
+++ src/constants/Providers.js
@@ -1,7 +1,8 @@
-import Config from 'react-native-config';
+//import Config from 'react-native-config';
 
 const ethers = require('ethers');
 
-const provider = new ethers.providers.InfuraProvider('ropsten', Config.INFURA_API_KEY);
+// const provider = new ethers.providers.InfuraProvider('ropsten', Config.INFURA_API_KEY);
+const provider = new ethers.providers.InfuraProvider('mainnet', 'O8UtwLH2uVXXIu89dieJ');
 
 export default provider;
