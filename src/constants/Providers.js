fd7fef09fb69ba4471de1d535916c50963dde043
Chris Fenos <chris.m.fenos@gmail.com>
2018-09-21 10:22:56 -0400

    react-native-config integrated for ios and android

--- src/constants/Providers.js
+++ src/constants/Providers.js
@@ -1,8 +1,7 @@
-//import Config from 'react-native-config';
+import Config from 'react-native-config';
 
 const ethers = require('ethers');
 
-// const provider = new ethers.providers.InfuraProvider('ropsten', Config.INFURA_API_KEY);
-const provider = new ethers.providers.InfuraProvider('mainnet', 'O8UtwLH2uVXXIu89dieJ');
+const provider = new ethers.providers.InfuraProvider('ropsten', Config.INFURA_API_KEY);
 
 export default provider;
