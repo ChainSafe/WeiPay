d56bbf19b579f9f414fadde6aa4d984114ea32df
Chris Fenos <chris.m.fenos@gmail.com>
2018-10-03 14:04:08 -0400

    saving

--- src/scripts/contracts/contractHelper.js
+++ src/scripts/contracts/contractHelper.js
@@ -31,7 +31,7 @@ const processContractByAddress = async (wallet, address) => {
     console.log(contract);
   } catch (err) {
     console.log(err);
-  } 
+  }
 };
 
 export default processContractByAddress;
